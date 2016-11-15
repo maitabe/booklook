af6e11ed95ea20085a20337de42c9826caa1d9a9
maitabe <martushb@gmail.com>
2016-11-15 13:54:21 +0200

    redesign UI

--- script.js
+++ script.js
@@ -1,19 +1,12 @@
-var books =
-	{title:'The Hunger Games',
-	 author:'Suzanne Collins',
-	 description:'Katniss Everdeen lives in a place called Panem. Panem lies on what used to be North America. Panem is split up into 12 districts. Each year, every district is forced to randomly choose two tributes--one girl and one boy--to fight in the annual Hunger Games.',
-	 time:25,
-	 pages:374,
-	 imgSrc:'img/hungerGames.jpg'
-	};
+// var books =
+// 	{title:'The Hunger Games',
+// 	 author:'Suzanne Collins',
+// 	 description:'Katniss Everdeen lives in a place called Panem. Panem lies on what used to be North America. Panem is split up into 12 districts. Each year, every district is forced to randomly choose two tributes--one girl and one boy--to fight in the annual Hunger Games.',
+// 	 time:25,
+// 	 pages:374,
+// 	 imgSrc:'img/hungerGames.jpg'
+// 	};
 
-
-//functions
-// var submitForm = function() {
-// 	if(){
-
-// 	}
-// };
 
 
 var source = $('#book-template').html();
