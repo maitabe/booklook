ebfb34e5bb42b5c8fb3ee0848dd385d0d5e2b4ef
maitabe <martushb@gmail.com>
2016-11-15 13:44:53 +0200

    adding event handler to search

--- script.js
+++ script.js
@@ -8,9 +8,33 @@ var books =
 	};
 
 
+//functions
+// var submitForm = function() {
+// 	if(){
+
+// 	}
+// };
 
 
 var source = $('#book-template').html();
 var template = Handlebars.compile(source);
 var newHtml = template(books);
-$('#info-book').append(newHtml);
+// $('#info-book').append(newHtml);
+
+
+//event handlers
+$(document).ready(function() {
+
+	$('#search-book').on('click', function(){
+
+		var title = $('#book-title').val();
+		var author = $('#book-author').val();
+		var description = $('#book-description').val();
+		var imgSrc = $('#book-url').val();
+		var pages = $('#book-no-pages').val();
+		var time = $('#book-read-time').val();
+
+		$('#info-book').append(newHtml);
+	});
+
+});
