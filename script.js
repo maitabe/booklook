c946961a7b633a1b6aa2b84d55db2d23b7dcc35b
maitabe <martushb@gmail.com>
2016-11-15 16:48:51 +0200

    update

--- script.js
+++ script.js
@@ -4,7 +4,6 @@
 var fetch = function(isbn) {
 	$.ajax({
 	  method: "GET",
-	  // url: 'https://www.googleapis.com/books/v1/volumes?q=isbn:0439023521',
 	  url: 'https://www.googleapis.com/books/v1/volumes?q=' + isbn,
 	  dataType: "json",
 	  success: function(data) {
